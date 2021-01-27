2e69742bb1561dbeb430c60812524a2c3bb22c85
Emre <kutukemre68@gmail.com>
2021-01-27 20:28:15 +0100

    Completing admin panel

--- admin/products.php
+++ admin/products.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <?php
+include("adminpartials/session.php");
 include("adminpartials/adminhead.php");
 ?>
 
@@ -24,34 +25,42 @@ include("adminpartials/adminhead.php");
                 </ol>
             </section>
             <div class="col-sm-3"></div>
-            <form role="form">
+            <form role="form" action="producthandler.php" method="post" enctype="multipart/form-data">
                 <div class=" col-sm-6">
                     <div class="box-body">
                         <div class="form-group">
                             <h1>Add a Product</h1>
                             <label for="productName">Name</label>
-                            <input type="text" class="form-control" id="productName" placeholder="Enter product name">
+                            <input type="text" class="form-control" id="productName" placeholder="Enter product name" name="productName">
                         </div>
                         <div class="form-group">
                             <label for="productPrice">Price</label>
-                            <input type="text" class="form-control" id="productPrice" placeholder="Enter product price">
+                            <input type="text" class="form-control" id="productPrice" placeholder="Enter product price" name="productPrice">
                         </div>
 
                         <div class="form-group">
                             <label for="productImage">Image</label>
-                            <input type="file" id="productImage">
+                            <input type="file" id="productImage" name="productImage">
                         </div>
                         <div class="form-group">
                             <label for="productCategory">Category</label>
-                            <select id="productCategory">
-                                <option>Mugs</option>
-                                <option>Tshirt</option>
-                                <option>Action Figure</option>
+                            <select id="productCategory" name="productCategory">
+                                <?php
+                                include("../partials/connect.php");
+                                $cat="SELECT * FROM categories";
+                                $results=mysqli_query($connect,$cat);
+
+                                while($row=mysqli_fetch_assoc($results))
+                                {
+                                    echo "<option value=".$row['id'].">".$row['name']."</option>";                                            
+                                }
+                                
+                                ?>
                             </select>
                         </div>
                             <div class="form-group">
                                 <label for="productDescription">Description</label>
-                                <textarea id="productDescription" class="form-control" rows="10" placeholder="Enter product description"></textarea>
+                                <textarea id="productDescription" class="form-control" rows="10" placeholder="Enter product description" name="productDescription"></textarea>
                             </div>
                         </div>
 
